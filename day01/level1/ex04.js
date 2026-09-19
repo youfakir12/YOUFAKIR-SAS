61c247508ef9061d9dffdc912cb7ef93bd812619
youfakir <oufakiryassine2@gmail.com>
2026-09-19 11:08:01 +0100

    Complete ex

---
+++ day01/level1/ex04.js
@@ -0,0 +1,12 @@
+let i = 1;
+while (i <= 50){
+    if (i % 3 === 0 && i % 5 === 0)
+        console.log("fizz buzz");
+    else if (i % 3 === 0)
+        console.log("fizz");
+    else if ( i % 5 === 0)
+        console.log("buzz");
+     else
+        console.log(i);
+    i++;
+}
